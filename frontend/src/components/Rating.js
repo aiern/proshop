43ebbf6f330dad40e6640c9b7188a5319ee39858
ai_ern <im_in@outlook.sg>
2020-11-28 21:21:28 +0800

    implement react router to prevent page refresh, single page app only

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -58,6 +60,17 @@ const Rating = ({ value, text, color }) => {
       <span>{text && text}</span>
     </div>
   )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string
+
 }
 
 export default Rating
