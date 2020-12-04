7f8f04dacde753bb758d27eb05c43c6570258250
ai_ern <im_in@outlook.sg>
2020-12-04 22:18:28 +0800

    data seeder script

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,19 +5,17 @@ const users = [
     name: 'Admin User',
     email: 'admin@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdmin: true
+    isAdmin: true,
   },
   {
     name: 'John Bada Bing',
     email: 'john@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdmin: true
   },
   {
     name: 'Jane Bada Boom',
     email: 'jane@example.com',
     password: bcrypt.hashSync('123456', 10),
-    isAdmin: true
   },
 
 ]
